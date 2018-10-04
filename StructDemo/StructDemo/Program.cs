fed7746f4a868e66d45cce473136b6b2cad9f318
Utkarsh Upadhyay <ut.101295@gmail.com>
2018-10-04 10:15:02 +0530

    Add files via upload

---
+++ StructDemo/StructDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p = new Point(2,5);
+            p.X += 100;
+            int px = p.X; // px=102
+
+            Console.WriteLine("The value of px :" + px);
+
+            Console.ReadKey();
+        }
+    }
+
+    public struct Point
+    {
+        int x, y;
+        public Point(int x,int y)
+            {
+            this.x = x;
+            this.y = y;
+            }
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+    }
+}
---
+++ UserDefinedexception/UserDefinedexception/EmployeeException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserDefinedexception
+{
+    class EmployeeException : ApplicationException
+    {
+        public EmployeeException() : base()
+        {
+
+        }
+        public EmployeeException(string message) : base(message)
+        {
+
+        }
+    }
+}
