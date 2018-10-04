c7bf58abb59e7aa22e54e59de3dae2644e7b32d8
Utkarsh Upadhyay <ut.101295@gmail.com>
2018-10-04 09:38:49 +0530

    Add files via upload

---
+++ AnonymousTypeDemo/AnonymousTypeDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousTypeDemo
+{
+    class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee() { ID = 101,Name = "Robert" };
+            object o = new Employee() { ID = 102, Name = "Maria" };
+            //((Employee)o).ID
+            //((Employee)o).Name
+            var v = new Employee() { ID = 103, Name = "Reena" };
+
+            object person = new { personName = "Allister", Age = 30 };
+            var person1 = new { personName = "Jason", Age = 30 };
+            var person2 = new { personName = "Tennyson", Age = 30 };
+            var person3 = new { Age = 30 , personName = "Daniel" };
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ArrayExample/ArrayExample/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[5];
+            Console.WriteLine("For Single dimension Array:");
+            Console.WriteLine("Enter the number one by one:");
+            for(int i=0;i<a.Length;i++)
+            {
+                //a[i] = int.Parse(Console.ReadLine());
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("The values entered are :");
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(i + " == " + a[i]);
+            }
+            Console.WriteLine("The SORTED array is :");
+            Array.Sort(a);
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(i + " == " + a[i]);
+            }
+            Array.Reverse(a);
+            Console.WriteLine("The REVERSE array is :");
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(i + " == " + a[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
