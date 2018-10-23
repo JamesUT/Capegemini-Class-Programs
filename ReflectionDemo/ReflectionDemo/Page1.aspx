905edbfa65da5aae3fb5833f7093e9eeadbfa3e7
Utkarsh Upadhyay <ut.101295@gmail.com>
2018-10-23 18:40:20 +0530

    Add files via upload

---
+++ ReflectionDemo/ReflectionDemo/Page1.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Page1.aspx.cs" Inherits="ReflectionDemo.Page1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <h1>Page 1 is Displayed</h1>
+        <asp:Button ID="btnRedirect" runat="server" Text="Response.Redirect" OnClick="btnRedirect_Click" />
+        <asp:Button ID="btnTransfer" runat="server" Text="Response.Transfer" OnClick="btnTransfer_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ ReflectionDemo/ReflectionDemo/Page1.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ReflectionDemo
+{
+    public partial class Page1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnRedirect_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Page2.aspx");
+        }
+
+        protected void btnTransfer_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("Page2.aspx");
+        }
+    }
+}
