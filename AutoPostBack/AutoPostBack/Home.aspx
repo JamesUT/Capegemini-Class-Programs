cd013ec59684b2a9d8b72abe5974031529020512
Utkarsh Upadhyay <ut.101295@gmail.com>
2018-10-23 18:26:52 +0530

    Add files via upload

---
+++ AutoPostBack/AutoPostBack/Home.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="AutoPostBack.Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <b>Select City :</b>
+        <asp:DropDownList ID="ddlCity" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddlCity_SelectedIndexChanged"></asp:DropDownList>
+        <div id="divCity" runat="server" visible="false">
+            <b>If others,Please Specify :</b>
+            <asp:TextBox id:"textCity" runat="server"></asp:TextBox>        
+        </div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ AutoPostBack/AutoPostBack/Home.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AutoPostBack
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!Page.IsPostBack)
+            {
+                ddlCity.Items.Add("Pune");
+                ddlCity.Items.Add("Mumbai");
+                ddlCity.Items.Add("Hyderabad");
+                ddlCity.Items.Add("Chennai");
+                ddlCity.Items.Add("Others");
+            }
+        }
+
+        protected void ddlCity_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(ddlCity.SelectedItem.Text == "Others")
+            {
+                divCity.Visible = true;
+            }
+            else
+            {
+                divCity.Visible = false;
+            }
+        }
+    }
+}
