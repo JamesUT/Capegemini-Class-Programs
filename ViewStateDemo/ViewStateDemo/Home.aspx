6787874ad55f61d9328e515ffa5a6ca9fc0ade51
Utkarsh Upadhyay <ut.101295@gmail.com>
2018-10-24 18:11:56 +0530

    Add files via upload

---
+++ ViewStateDemo/ViewStateDemo/Home.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="ViewStateDemo.Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <b>Counter value :</b> <asp:Label ID="lblCounter" runat="server"></asp:Label>
+        <br /><br />
+        <asp:Button ID="btnShow" runat="server" Text="Show" OnClick="btnShow_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ ViewStateDemo/ViewStateDemo/Home.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ViewStateDemo
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                ViewState["Counter"] = 0;
+                lblCounter.Text = ViewState["Counter"].ToString();
+            }
+        }
+
+        protected void btnShow_Click(object sender, EventArgs e)
+        {
+            int cnt = (int) ViewState["Counter"];
+            cnt++;
+            ViewState["Counter"] = cnt;
+            lblCounter.Text = ViewState["Counter"].ToString();
+        }
+    }
+}
