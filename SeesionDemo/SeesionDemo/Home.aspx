b98205179ccdb9ad4076afecad630e992ec010b2
Utkarsh Upadhyay <ut.101295@gmail.com>
2018-10-24 18:11:52 +0530

    Add files via upload

---
+++ SeesionDemo/SeesionDemo/Home.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="SeesionDemo.Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <asp:Label ID="lblSessionID" runat="server"></asp:Label>
+        <br /><br />
+        <b>Session Counter :</b>
+        <asp:Label ID="lblSessionCounter" runat="server"></asp:Label>
+        <br /> <br />
+        <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ SeesionDemo/SeesionDemo/Home.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SeesionDemo
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!Page.IsPostBack)
+            {
+                Session["counter"] = 0;
+                lblSessionID.Text = Session.SessionID;
+                lblSessionCounter.Text = Session["counter"].ToString();
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            int cnt = (int)Session["counter"];
+            cnt = cnt + 1;
+            Session["counter"] = cnt;
+            lblSessionCounter.Text = Session["counter"].ToString();
+        }
+    }
+}
